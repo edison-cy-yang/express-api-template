6c585c1b2949cca895bb56ce2738b948d5df56e3
edison-cy-yang <edison.cy.yang@gmail.com>
2020-03-01 18:30:45 -0800

    template setup

---
+++ README.md
@@ -0,0 +1,25 @@
+Watch Buddy API
+=========
+
+
+## Getting Started
+
+1. Create the `.env` by using `.env.example` as a reference: `cp .env.example .env`
+2. Update the .env file with your correct local information 
+  - username: `` 
+  - password: `` 
+  - database: ``
+3. Install dependencies: `npm i`
+4. Fix to binaries for sass: `npm rebuild node-sass`
+5. Reset database: `npm run db:reset`
+  - Check the db folder to see what gets created and seeded in the SDB
+7. Run the server: `npm run local`
+  - Note: nodemon is used, so you should not have to restart your server
+8. Visit `http://localhost:8080/`
+
+
+## Dependencies
+
+- Node 10.x or above
+- NPM 5.x or above
+- PG 6.x
---
+++ src/server.js
@@ -0,0 +1,48 @@
+// // load .env data into process.env
+// require('dotenv').config();
+
+// Web server config
+const PORT       = process.env.PORT || 8080;
+const ENV        = require('./environment');
+const express    = require("express");
+const bodyParser = require("body-parser");
+const app        = express();
+const morgan     = require('morgan');
+
+// PG database client/connection setup
+const { Pool } = require('pg');
+const dbParams = require('./lib/db.js');
+const db = new Pool(dbParams);
+db.connect();
+
+// Load the logger first so all (static) HTTP requests are logged to STDOUT
+// 'dev' = Concise output colored by response status for development use.
+//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+// Separated Routes for each Resource
+// Note: Feel free to replace the example routes below with your own
+const usersRoutes = require("./routes/users");
+const widgetsRoutes = require("./routes/widgets");
+
+// Mount all resource routes
+// Note: Feel free to replace the example routes below with your own
+app.use("/api/users", usersRoutes(db));
+app.use("/api/widgets", widgetsRoutes(db));
+// Note: mount other resources here, using the same pattern above
+
+
+// Home page
+// Warning: avoid creating more routes in this file!
+// Separate them into separate routes files (see above).
+app.get("/", (req, res) => {
+  res.send("{hello: world}");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT} in ${ENV}`);
+});
